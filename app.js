9434a5ef2467946154e679e58e9147a830b088c8
John Hansen <jrwh81@gmail.com>
2015-01-26 17:50:38 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var showApp = angular.module('securityApp', ['ngAnimate'])
+	.controller('mainController', function($scope) {
+		$scope.info = "hide";
+		$scope.showMoreInfo = function(){
+			$scope.info = "show";
+		}
+		$scope.showLessInfo = function(){
+			$scope.info = "hide";
+		}
+
+		$scope.currentIP = "192.168.0.1";
+
+		$.get("http://jsonip.com", function(r){
+			$scope.currentIP = r.about;
+		});		
+});
